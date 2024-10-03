30616c5506399996730eb477cacc890c5909ae83
Shihan <sharifulislamshihan@gmail.com>
2024-10-03 21:17:40 +0600

    home page added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,19 @@
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+
+    const navigate = useNavigate();
+
+    const handleStart = () =>{
+        navigate('/role');
+    }
     return (
-        <div>
-            <h3 className="font-arima">This is Home</h3>
+        <div className="bg-[url('/home.png')] bg-cover bg-center min-h-screen flex flex-col justify-end items-center md:bg-cover sm:bg-cover">
+                <button
+                onClick={handleStart} 
+                className="text-center py-10">
+                  <img className="w-1/2 mx-auto" src="/startNow.png" alt="start button" />
+                </button>
         </div>
     );
 };
